394477cf514896123345ec41c265539113b156cf
AmirMPakdel <amd.amp1969@gmail.com>
2021-10-27 09:19:24 +0330

    working on laravel 2

--- utils/mainServer.js
+++ utils/mainServer.js
@@ -1,5 +1,6 @@
 const { default: axios } = require("axios");
 
+/**@param {import("express").Request} req*/
 function uploadKeyCheck(req, res, cb){
 
 
@@ -7,13 +8,18 @@ function uploadKeyCheck(req, res, cb){
 
     let p = {
         token: b.token,
+        tenant: b.tenant,
         upload_key: b.upload_key,
         file_type: b.file_type,
         upload_type: b.upload_type, //(exp. educator_avatar)
         course_id: b.course_id, //(nullable)
     }
 
-    axios.post("http://tootifa.ir/api/tenant/user/upload/verify", p, {}).then(res2=>{
+    axios.post("http://tootifa.ir/api/tenant/user/upload/verify", p, {
+        headers:{
+            "X-Tenant":tenant
+        }
+    }).then(res2=>{
 
         cb(null, res2);
 
